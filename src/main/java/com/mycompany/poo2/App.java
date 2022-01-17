7a02aa88fa265fb3488c0da8c70f2e3ef7367b7a
VanegasMaykoll <94266540+VanegasMaykoll@users.noreply.github.com>
2022-01-17 12:01:33 -0500

    Prueba

--- src/main/java/com/mycompany/poo2/App.java
+++ src/main/java/com/mycompany/poo2/App.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-//probando maykoll commit
+//probando maykoll 
 /**
  * JavaFX App
  */
